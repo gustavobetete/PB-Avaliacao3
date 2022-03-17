8b249510afdf3d2200931b07fb90b6c57ed5a606
Gustavo Betete <gustavobetete@hotmail.com>
2022-03-17 10:02:52 -0300

    Foi adicionado a anotação @EnableSpringDataWebSupport

--- exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/Exercicio1Application.java
+++ exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/Exercicio1Application.java
@@ -2,8 +2,10 @@ package br.com.alura.exercicio1.exercicio1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class Exercicio1Application {
 
 	public static void main(String[] args) {

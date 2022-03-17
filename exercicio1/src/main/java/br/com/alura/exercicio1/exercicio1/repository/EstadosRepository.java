563989a3d15f814ce96d16b94e410d79e6833cda
Gustavo Betete <gustavobetete@hotmail.com>
2022-03-17 10:12:36 -0300

    Mudado o findByRegiao para Page<Estado>

--- exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/repository/EstadosRepository.java
+++ exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/repository/EstadosRepository.java
@@ -2,15 +2,13 @@ package br.com.alura.exercicio1.exercicio1.repository;
 
 import br.com.alura.exercicio1.exercicio1.modelo.Estado;
 import br.com.alura.exercicio1.exercicio1.modelo.StatusEstado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 public interface EstadosRepository extends JpaRepository<Estado, Long> {
 
-    List<Estado> findByNome(String nome);
-
-    List<Estado> findByRegiao(StatusEstado regiao);
+   Page<Estado> findByRegiao(StatusEstado regiao, Pageable paginacao);
 }
 

2a9a8cd75d5954f7eca380395c1e088d248d4d81
Gustavo Betete <gustavobetete@hotmail.com>
2022-03-17 10:06:30 -0300

    troquei o List<EstadoDto> para Page<EstadoDto> para assim fazer uma paginacao e ordenacao

--- exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/controller/EstadosController.java
+++ exercicio1/src/main/java/br/com/alura/exercicio1/exercicio1/controller/EstadosController.java
@@ -6,6 +6,10 @@ import br.com.alura.exercicio1.exercicio1.modelo.Estado;
 import br.com.alura.exercicio1.exercicio1.modelo.StatusEstado;
 import br.com.alura.exercicio1.exercicio1.repository.EstadosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,7 +17,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,15 +28,18 @@ public class EstadosController {
     private EstadosRepository estadoRepository;
 
     @GetMapping // mesma url porem agora com metodo Get
-    public List<EstadoDto> lista(StatusEstado regiao) {
+    public Page<EstadoDto> lista(@RequestParam(required = false) StatusEstado regiao, @PageableDefault(sort="id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao){
+                                                                                //@PageableDefault serve para indicar o padrão de paginação/ordenação ao Spring, quando o cliente da API não enviar tais informações
         if (regiao == null) {
-            List<Estado> estados = estadoRepository.findAll();
+            Page<Estado> estados = estadoRepository.findAll(paginacao);
             return EstadoDto.converter(estados);
-        } else {
-            List<Estado> estados = estadoRepository.findByRegiao(regiao);
+        }else {
+            Page<Estado> estados = estadoRepository.findByRegiao(regiao, paginacao);
             return EstadoDto.converter(estados);
         }
+
     }
+
     @PostMapping // mesma url porem agora com metodo Post
     @Transactional // se não fizer o @Transactional ele não atualiza o commit no banco de dados
     public ResponseEntity<Estado> cadastrar(@RequestBody @Valid Estado estado, UriComponentsBuilder uriBuilder){ // RequestBody indica ao String que os parâmetros enviados no corpo da requisição devem ser atribuidos ao parâmetro do método
@@ -43,6 +49,7 @@ public class EstadosController {
         URI uri = uriBuilder.path("/api/states").buildAndExpand(estado.getId()).toUri();
         return ResponseEntity.created(uri).body(estado);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Estado> detalhar(@PathVariable Long id){
 
@@ -52,6 +59,7 @@ public class EstadosController {
         }
         return ResponseEntity.notFound().build();
     }
+    
     @PutMapping("/{id}") // put sobrescreve / atualiza todas informações que ja estão escritas.
     @Transactional
     public ResponseEntity<EstadoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoEstadoForm form ){
